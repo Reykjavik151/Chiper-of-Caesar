91071337e1f47f1d241170b0eb17a750e8895fcd
Unlit <Reykjavik151@gmail.com>
2017-05-31 16:50:08 +0300

    Cipher of Caesar v0.4

---
+++ CipherOfCaesar/CipherOfCaesar/Cleaner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherOfCaesar
+{
+    static class Cleaner
+    {
+        static public string CleanWord(string word)
+        {
+            string result = "";
+            foreach(char Ch in word)
+            {
+                if ((96 <= Ch && Ch <= 122) || Ch == '-' || Ch == '/')
+                    result += Ch;
+            }
+            return result;
+        }
+    }
+}
--- CipherOfCaesar/CipherOfCaesar/MainWindow.xaml.cs
+++ CipherOfCaesar/CipherOfCaesar/MainWindow.xaml.cs
@@ -41,6 +41,45 @@ namespace CipherOfCaesar
         {
             Bridge(false);
         }
+        private void richTextBoxEnterOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string[] items = (new TextRange(richTextBoxEnter.Document.ContentStart,
+                                            richTextBoxEnter.Document.ContentEnd).Text).ToLower().Split(' ');
+
+            for(int i = 0; i < items.Length; i++)
+                items[i] = Cleaner.CleanWord(items[i]);
+
+            if (items.Length == 0)
+                Guess(0);
+
+            Dictionary<int, int> Mathes = new Dictionary<int, int>();
+            Mathes.Add(0, items.Length);
+            foreach (string item in items)
+            {
+                cipher.Enter = item;
+                int guess = cipher.Guess();
+                if (guess != 0)
+                {
+                    if (Mathes.ContainsKey(guess))
+                        Mathes[guess]++;
+                    else
+                        Mathes[guess] = 1;
+
+                    Mathes[0]--;
+                }
+            }
+
+            int value = Mathes.Max(n => n.Value);
+            foreach(KeyValuePair<int, int> buff in Mathes)
+            {
+                if (buff.Value == value)
+                {
+                    Guess(-buff.Key);
+                    return;
+                }
+            }
+            Guess(0);
+        }
 
         #endregion
 
@@ -63,11 +102,11 @@ namespace CipherOfCaesar
             }
             catch (FormatException)
             {
-                MessageBox.Show("Please, enter only integer number in the field \"Rotate to\"");
+                MessageBox.Show("Please, enter only integer number in the field \"Rotate to\".");
             }
-            catch(OverflowException)
+            catch (OverflowException)
             {
-                MessageBox.Show("");
+                MessageBox.Show("You have set too many cycles.");
             }
             
         }
@@ -85,6 +124,22 @@ namespace CipherOfCaesar
             richTextBoxAnswer.Document = document;
         }
 
+        /// <summary>
+        /// Функция смены текстового блока в зависимости от угадывания смещения
+        /// </summary>
+        private void Guess(int ROTN)
+        {
+            if (ROTN == 0)
+            {
+                textBlockGuess.Text = "I think, \nthat the word\nis not \nencrypted";
+                return;
+            }
+
+            textBlockGuess.Text = "Possible \ndecryption \nwith a rotate\nof " + (26 + ROTN);
+        }
+
         #endregion
+
+        
     }
 }
